e23a2bfda9e42cd995e2d1c045945e3e20a582e8
Elide <zavalavinagreelide@gmail.com>
2023-08-26 23:48:13 -0600

    🤠 string methods

---
+++ 5.string-methods.py
@@ -0,0 +1,83 @@
+# --------------------- #
+""" String Methods """
+# --------------------- #
+
+# 1- len()
+# len nos permite saber la longitud de un string
+address = "Tabasco"
+print(len(address))
+
+# [] notation
+# Nos permite acceder a un caracter en especifico
+print('------------------------------------')
+print(address[0])
+print(address[6])
+print(address[-1])
+print(address[-4])
+
+# [ : ] notation
+# Nos permite acceder a un rango de caracteres
+print('------------------------------------')
+print(address[0:3])
+print(address[-7:-1])
+print(address[0:])
+print(address[:7])
+print(address[:])
+
+country = "USA"
+city = "New york"
+full_address = country + ", " + city
+print(full_address)
+
+# 5- upper()
+# Nos permite convertir un string a mayusculas
+print('------------------------------------')
+print(full_address.upper())
+print(city[0] + city[1].upper() + city[2:7] + city[7].upper())
+
+# 6- lower()
+# Nos permite convertir un string a minusculas
+print('------------------------------------')
+print(city.lower())
+
+# 7- title()
+# Nos permite convertir un string a titulo
+print('------------------------------------')
+print(full_address.title())
+
+# 8- strip()
+# Nos permite eliminar espacios en blanco
+print('------------------------------------')
+job = "     Developer     "
+print(job.strip())
+print(job.rstrip())  # Elimina espacios en blanco a la derecha
+print(job.lstrip())  # Elimina espacios en blanco a la izquierda
+
+# 9- find()
+# Nos permite encontrar un caracter o una palabra en un string
+# Si no encuentra el caracter o la palabra, regresa -1
+print('------------------------------------')
+print(address.find("o"))
+print(address.find("a"))
+print(address.find("r"))
+print(address.find("T"))
+
+# 10- replace()
+# Nos permite reemplazar un caracter o una palabra en un string
+print('------------------------------------')
+print(address.replace("a", "u"))
+print(address.replace("Tabasco", "Mexico"))
+
+# 11- in operator
+# Nos permite saber si un caracter o una palabra esta en un string
+# Regresa un booleano
+print('------------------------------------')
+print("a" in city)
+print("york" in city)
+
+# 12- not in operator
+# Nos permite saber si un caracter o una palabra no esta en un string
+# Regresa un booleano
+print('------------------------------------')
+print("a" not in city)
+print("york" not in city)
