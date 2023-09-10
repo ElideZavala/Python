e7df71356b58076b27657a915ab5cbe973fba3d3
Elide <zavalavinagreelide@gmail.com>
2023-09-09 21:23:31 -0600

    Number operator part 1

---
+++ 8.number-methods_operations.py
@@ -0,0 +1,22 @@
+# --------------------- #
+""" Number Methods & Operations """
+# --------------------- #
+
+# ---------- Arithmetic Operations ----------- #
+# print(23 + 12)  # 35
+# print(50 - 10)  # 11
+# print(50 * 10)  # 500
+# print(25 / 10)  # 2.5 division
+# print(50 // 10)  # 5 division entera
+# print(50 % 10)  # 0 resto de una division
+# print(2 ** 5)  # 32 potencia
+
+# -------- Augmented Assignment Operator --------- #
+# +=, -=, *=, /=, //=, %=, **=
+
+# numer = 10
+# # numer = numer + 5
+# # numer += 5
+# # numer *= 5
+# # numer /= 5
+# print(numer)  # 15
