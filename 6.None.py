dcd0324ac36156a5be88919da9ec4f377ea8a06b
Elide <zavalavinagreelide@gmail.com>
2023-09-09 20:38:02 -0600

    variable None

---
+++ 6.None.py
@@ -0,0 +1,11 @@
+# --------------------- #
+""" None """
+# --------------------- #
+
+# The Absence of a Value
+
+print(None)  # None
+print(type(None))  # <class 'NoneType'>
+
+user_password = None
+print(user_password)  # None
