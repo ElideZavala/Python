6e35b2c525d15c9808862e401dfaef3f7803185b
Elide <zavalavinagreelide@gmail.com>
2023-09-09 20:56:55 -0600

    Metodos de Escape

---
+++ 7.escape_sequences.py
@@ -0,0 +1,11 @@
+# --------------------- #
+""" Escape Sequences """
+# --------------------- #
+
+# story_desc = "It's a touching story"
+# story_desc = "It\'s a touching story"
+# story_desc = 'It\'s a touching story'
+# story_desc = "It\\'s a touching story" # para que se imprima la barra invertida
+story_desc = "It's a \n touching \n story" # por cada \n se hace un salto de linea
+
+print(story_desc)
