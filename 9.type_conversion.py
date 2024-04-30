2f36d6b509d1255ea8eecf639f67a0b9edaaf153
Elide <zavalavinagreelide@gmail.com>
2024-04-29 20:42:47 -0600

    Agregadas conversiones de tipo y ejemplos de uso

--- 9.type_conversion.py
+++ 9.type_conversion.py
@@ -2,5 +2,20 @@
 """ Number Methods & Operations """
 # --------------------- #
 
-number = input("number: ")
-print(type(number))  # <class 'str'>
+# number = input("number: ")
+# y = int(number) + 202
+# print(y)  # 212
+# print(type(y))  # <class 'int'> 
+
+#int() - convert to integer
+# float() - convert to float
+# str() - convert to string
+#bool() - convert to boolean
+
+print(bool(0))  # False
+print(bool("")) # False
+print(bool(None)) # False
+
+print(bool(10)) # True
+print(bool("hello")) # True
+print(bool("False")) # True
