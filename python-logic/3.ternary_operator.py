4a90c412662295c27311faaf9dfae7184f86f1fa
Elide <zavalavinagreelide@gmail.com>
2024-05-07 20:39:40 -0600

    Agregado operador ternario para evaluar el resultado del puntaje

---
+++ python-logic/3.ternary_operator.py
@@ -0,0 +1,20 @@
+
+# *_*_*_*_*__*_*_*_*_*_* First Version
+score = 74
+# if score >= 75:
+#     print("You passed")
+# else:
+#     print("You failed")
+
+# *_*_*_*_*__*_*_*_*_*_* Second Version
+# if score >= 75:
+#     result = "You passed"
+# else:
+#     result = "You failed"
+
+# print(result)
+
+# mas profecional y corto
+# *_*_*_*_*__*_*_*_*_*_* Second Version >>> Ternary Operator
+result = "Passed" if score >= 75 else "failed"
+print(result)
