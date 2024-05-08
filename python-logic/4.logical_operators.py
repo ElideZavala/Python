d93c4078df9a5af632cdae419881b1fe5d9c0f3a
Elide <zavalavinagreelide@gmail.com>
2024-05-07 20:39:49 -0600

    Actualización de la función de búsqueda***

---
+++ python-logic/4.logical_operators.py
@@ -0,0 +1,36 @@
+# ------------------------------------------
+# ---------- **## Logical Operators ##** ----------
+
+"""
+1- and
+2- or
+3- not
+"""
+
+# Example 1 ->> Processing loans ->>>> and Lo
+# high_income = True
+# good_credit = True
+
+# if high_income and good_credit:
+#     print("Eligible for loan")
+# else:
+#     print("Not eligible for loan")
+
+# Example 2 ->> Processing loans ->>>> or LO
+# high_income = True
+# good_credit = False
+
+# if high_income or good_credit:
+#     print("Eligible for loan")
+# else:
+#     print("Not eligible for loan")
+
+# Example 3 ->> Processing loans ->>>> not LO
+# high_income = True
+# good_credit = False
+student = False
+
+if not student:
+    print("Eligible for loan")
+else:
+    print("Not eligible for loan")
---
+++ python-loops/for_loops_pt.py
@@ -0,0 +1,23 @@
+# ----------------------------------------------
+# ---------------- **## For Loops ##** ----------------
+
+# ********___Example 1___********
+# print("Success")
+# print("Success")
+
+# ********___Example 2___********
+# for x in range(10):
+#     print(x)
+
+# ********___Example 3___********
+# for number in range(5):
+#     print(f"The code has run for {number} time(s)")
+# f sirve para concatenar variables con strings
+
+# ********___Example 4___********
+# for a in range(5, 15):
+#     print(a)
+
+# ********___Example 5___********
+for a in range(0, 100, 15):
+    print(a)
