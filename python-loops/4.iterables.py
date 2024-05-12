7addf816a77682a3fd1bf1d799772516281bbf66
Elide <zavalavinagreelide@gmail.com>
2024-05-12 17:40:33 -0600

    Agregado bucle for para iterar sobre una lista de actividades

---
+++ python-loops/4.iterables.py
@@ -0,0 +1,18 @@
+# ------------------------------------------------------------------------------------
+# -----------------------------**## Iterables ##**-----------------------------------
+
+# ******_____ Example 10 _____******
+# print(type(range(5)))
+
+# ******_____ Example 11 _____******
+
+# for c in "Elide Zavala Vinagre":
+#     print(c)
+
+# char es una variable temporal que se crea en el momento de la ejecucion del for loop para almacenar el valor de cada uno de los elementos de la cadena de texto
+
+# ******_____ Example 12 _____******
+something = []
+
+for something in ["Coffe", "Play with the Cat", "Walk the dog"]:
+    print(something)
