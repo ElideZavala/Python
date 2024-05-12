09d702a3a2f8c1ac28fd2e7d46ae68e806257d01
Elide <zavalavinagreelide@gmail.com>
2024-05-12 17:40:39 -0600

    Agregado bucle while para imprimir la temperatura descendente

---
+++ python-loops/5.while_loops.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------
+# ------------- **## While Loops ##** --------------
+
+
+# ********___Example 13___********
+temperature = 40
+
+while temperature >= 20:
+    print(
+        f"It es a good day to go out when the temperature is {temperature}")
+    temperature -= 5
+
+# aqui se esta decrementando la variable temperature en 5, por lo que el while loop se ejecutara 5 veces, ya que la condicion es que la temperatura sea mayor o igual a 20
