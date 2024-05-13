0000fde10476524b27474a376d35915a33cd54c6
Elide <zavalavinagreelide@gmail.com>
2024-05-12 18:23:32 -0600

    Actualización de la función de búsqueda***

---
+++ Functions/1.intro.py
@@ -0,0 +1,32 @@
+# -----------------------------------------------------------
+# --------------- **## Functions ##** ----------------------
+
+# *******------ Ejemplo 1 ------*******
+def book():
+    print("I am a book")
+
+
+book()
+
+# *******------ Ejemplo 2 ------*******
+success = False
+
+
+def operation():
+    if success:
+        print("Yay !!!")
+    else:
+        print("Oh no !!!")
+
+
+operation()
+
+# *******------ Ejemplo 3 ------*******
+
+
+def sum():
+    for number in range(4, 34, 4):
+        print(number)  # acuerdate que el ultimo numero no se incluye en el rango
+
+
+sum()
