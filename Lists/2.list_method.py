1b321922dc455bb14e21976b1a0be9345604b8b5
Elide <zavalavinagreelide@gmail.com>
2024-05-26 16:04:57 -0600

    Agregados ejemplos de métodos de listas en el archivo 2.list_method.py

---
+++ Lists/2.list_method.py
@@ -0,0 +1,17 @@
+# -------------------------------------------------------------------------------------------
+# ---------------------------**## The list methods ##**-------------------------------------
+
+# ! ******** Example 6 ********
+numbers = list(range(100))
+# print(numbers)
+
+# ! ******** Example 7 ********
+randon_name = list("Isabella")
+# pasa por que el string es un iterable
+print(randon_name)
+
+# ! ******** Example 8 ********
+print(len(randon_name))  # 8 # la longitud de la lista
+print(len(numbers))  # 100
+
+print(numbers)
