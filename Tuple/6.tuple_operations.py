bfde7f034971f5ce621b2b4d1a918ec68382a75c
Elide <zavalavinagreelide@gmail.com>
2024-06-12 21:18:52 -0600

    feat: Add tuple operations code and examples

---
+++ Tuple/6.tuple_operations.py
@@ -0,0 +1,16 @@
+# -------------------------------------------------------------------------------------------------
+# ----------------------------------**## Tuple Operations ##**-------------------------------------
+
+colors = ('red', 'green', 'blue', 'yellow', 'black', 'white')
+
+#! ************ Example 18 ************ Tuple Membership Test ************
+# print("green" in colors)  # True
+# print("orange" in colors)  # False
+# print("red" in colors)  # True
+
+#! ************ Example 19 ************ Tuple over tuples ************
+for color in colors:
+    print(color)
+    # output: red green blue yellow black white
+
+# print the elements of the tuple
