39e183b7fe974ec91fed8ec661689ab586650900
Elide <zavalavinagreelide@gmail.com>
2024-07-09 21:58:44 -0600

    feat: Add with statement example and explanation

---
+++ Error-Handling/5.the_with_statement.py
@@ -0,0 +1,35 @@
+# -------------------------------------------------------------------------------------
+# ***** The with Statement *****
+
+#! Opening a single fileb
+print("******************* Example 15 *********************")
+
+# __exit__
+
+# try:
+#     with open("someFil.txt") as note:
+#         print("File opened..." + note.name)
+
+#     age = int(input("Age: "))
+#     x = 10 / age
+# except (ValueError, ZeroDivisionError):
+#     print("Plese enter a valid age")
+# except FileNotFoundError:
+#     print("oops")
+# else:
+#     print("No Exceptions Here")
+
+#! Opening multiple files
+print("******************* Example 16 *********************")
+# try:
+#     with open("someFile.txt") as note, open("anotherFile.txt") as my_note:
+#         print("File opened..." + note.name + " & " + my_note.name)
+
+#     # age = int(input("Age: "))
+#     # x = 10 / age
+# except (ValueError, ZeroDivisionError):
+#     print("Plese enter a valid age")
+# except FileNotFoundError:
+#     print("file was not found...")
+# else:
+#     print("No Exceptions Here")
