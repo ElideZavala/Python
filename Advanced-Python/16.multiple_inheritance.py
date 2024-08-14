75fe5b7185a0fb9028454a48167ea1e110b6dbf6
Elide <zavalavinagreelide@gmail.com>
2024-08-13 22:06:17 -0600

    feat: Add multiple inheritance example and explanation

---
+++ Advanced-Python/16.multiple_inheritance.py
@@ -0,0 +1,46 @@
+# ------------------------------------------------------------------
+# Multiple Inheritance:
+
+#! Bad Multiple Inheritance
+print("******************** Example 39 ********************")
+
+
+# class Person:
+#     def sport_status(self):
+#         print("Runner")
+
+
+# class John:
+#     def sport_status(self):
+#         print("jogger")
+
+
+# class Jane(Person, John):
+#     pass
+
+
+# jane = Jane()
+# jane.sport_status()
+
+# jane = Jane()
+#! Goof Multiple Inheritance
+print("******************** Example 40 ********************")
+
+
+class Person:
+    def runner(self):
+        print("Runner")
+
+
+class John:
+    def jogger(self):
+        print("jogger")
+
+
+class Jane(Person, John):
+    pass
+
+
+jane = Jane()
+jane.runner()
+jane.jogger()
