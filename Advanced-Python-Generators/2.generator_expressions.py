199e0cc5c846cc9ef41ef97637791d5831d9af8c
Elide <zavalavinagreelide@gmail.com>
2024-08-28 21:39:06 -0600

    feat: Add generator expressions examples and explanations

---
+++ Advanced-Python-Generators/2.generator_expressions.py
@@ -0,0 +1,6 @@
+# -----------------------------------------------------------------------------
+#! Generator Expressions
+
+# ? Example 3 creating a generator expression
+
+# ? Example 4 generator expression as function argument
