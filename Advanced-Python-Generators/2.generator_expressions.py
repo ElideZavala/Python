605627cdbbfd688c2090cf3031a6c85086e23554
Elide <zavalavinagreelide@gmail.com>
2024-09-01 20:56:43 -0600

    chore: Update generator expressions example code and explanations

--- Advanced-Python-Generators/2.generator_expressions.py
+++ Advanced-Python-Generators/2.generator_expressions.py
@@ -2,5 +2,23 @@
 #! Generator Expressions
 
 # ? Example 3 creating a generator expression
+numbers = [1, 2, 11, 9, 5, 2, 0, 1, -1]
+
+# squaring each term using list comprehension
+# sqrt_nums_LC = (x**2 for x in numbers)
+
+# print(next(sqrt_nums_LC)) # 1
+# print(next(sqrt_nums_LC)) # 4
+# print(next(sqrt_nums_LC)) # 121
+# print(next(sqrt_nums_LC)) # 81
 
 # ? Example 4 generator expression as function argument
+
+# obtaining the sum of the squares of the numbers
+print(sum(x * 2 for x in numbers))  # 60
+
+# obtaining the maximum of the squares of the numbers
+print(max(x ** 2 for x in numbers))  # 144
+
+# obtaining the minimum of the squares of the numbers
+print(min(x for x in numbers))  # 0
