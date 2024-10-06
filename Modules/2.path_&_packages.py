1d00b92af945a3d012e376511e7e56f20821e6da
Elide <zavalavinagreelide@gmail.com>
2024-10-05 20:57:50 -0600

    feat: Add module import examples and explanations

---
+++ Modules/2.path_&_packages.py
@@ -0,0 +1,27 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# ? Path & Packages
+
+# ----------****** The Path Object ******----------
+from social_media.user_imgs import img, media
+import social_media.user_imgs
+import sys
+# print(sys.path)
+
+# ----------****** Packages ******----------
+
+
+# ----------****** Importing Ways ******----------
+# Approach 1
+# social_media.user_imgs.img()
+
+
+# Approach 2
+
+# img()
+# media()
+
+# Approach 3
+from social_media import user_imgs
+
+user_imgs.img()  # User Images
+user_imgs.media()  # User Media
