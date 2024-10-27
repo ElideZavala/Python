c0ee4b21a3ae09b11eb99eeed7c2977290c48a01
Elide <zavalavinagreelide@gmail.com>
2024-10-27 12:31:39 -0600

    Add code examples to demonstrate different types of exceptions in Python

---
+++ Extracted/Error-Handling/1.what_are_exceptions.py
@@ -0,0 +1,18 @@
+# -----------------------------------------------------------------------------------
+# What are Exceptions?
+
+# Syntax Error
+# Logical Error( Exceptions )
+
+print("******************** Example 1 ********************")
+# if 3 > 1  # Syntax Error for missing colon
+
+print("******************** Example 2 ********************")
+# 1/0  # Traceback (most recent call last): ZeroDivisionError: division by zero
+
+print("******************** Example 3 ********************")
+# numbers = [1, 2]
+# print(numbers[3]) # IndexError: list index out of range
+
+print("******************** Example 4 ********************")
+age = int(input("age:"))
