aa7cb1f4ed615ec91a0d99c527dd2b5b12ea01f4
Elide <zavalavinagreelide@gmail.com>
2025-05-13 21:55:20 -0600

    question 2

--- Category 3 - Loops/2.question.py
+++ Category 3 - Loops/2.question.py
@@ -1,4 +1,12 @@
 # Print the following pattern
+lista = []
+
+for i in range(1):
+    for j in range(5):
+        
+        j += 1
+        lista.append(j )
+        print(" ".join(map(str, lista)))
 
 """
 1 
@@ -7,3 +15,10 @@
 1 2 3 4 
 1 2 3 4 5
 """
+print()
+last_num = 10
+
+for row in range(1, last_num):
+    for column in range(1, row + 1):
+        print(column, end=" ")
+    print("")
