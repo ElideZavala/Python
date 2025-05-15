d132e76ea541f0f44d20a68f406e69c05be6708b
Elide <zavalavinagreelide@gmail.com>
2025-05-14 23:12:32 -0600

    question 4

--- Category 3 - Loops/4.question.py
+++ Category 3 - Loops/4.question.py
@@ -1,1 +1,7 @@
 # Print the multiplication table for a number
+def multiplication_table(number):
+
+    for i in range(1, 10 + 1, 1):
+        print(f"{number} * {i} = {number * i}")
+
+multiplication_table(5)
