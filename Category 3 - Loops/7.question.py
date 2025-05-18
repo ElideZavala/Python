ee856e2e53073ee30adba947d603f586a37508d9
Elide <zavalavinagreelide@gmail.com>
2025-05-18 17:05:57 -0600

    question 7

--- Category 3 - Loops/7.question.py
+++ Category 3 - Loops/7.question.py
@@ -7,3 +7,21 @@
 2 1 
 1 
 """
+
+# Solution Propia
+
+number = 0 
+for i in range(6, 0, -1):
+    for j in range(i - 1, 0, -1):
+        print(j, end=" ")
+    print(" ")
+
+num1 = 5
+num2 = 5
+
+# Solution 
+for i in range(0, num1 + 1 ):
+    # print(i)
+    for j in range(num2 - i, 0, -1):
+        print(j, end=" ") 
+    print("")
