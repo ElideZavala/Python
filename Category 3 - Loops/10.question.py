e81f3cafe3e87734372521f0fae71f1dd7c12807
Elide <zavalavinagreelide@gmail.com>
2025-05-18 22:31:17 -0600

    question 10

--- Category 3 - Loops/10.question.py
+++ Category 3 - Loops/10.question.py
@@ -1,1 +1,17 @@
 # Display a message “Done” after successful execution of a for loop
+# Forma Propia
+try:
+    for i in range(5):
+        print(i)
+except:
+    print("Ocurrio un error en el proceso")
+finally:
+    print("Hecho")
+
+print("")
+
+# Forma orginal
+for j in range(5):
+    print(j)
+else: 
+    print("Done!!!")
