83e56f937b516f7a6605cc06b9c05192877e8b52
Elide <zavalavinagreelide@gmail.com>
2025-05-18 22:15:42 -0600

    question 8

--- Category 3 - Loops/8.question.py
+++ Category 3 - Loops/8.question.py
@@ -1,3 +1,22 @@
 # Reverse the following list using for loop
 
 list = [10, 20, 30, 40, 50]
+new_list = []
+
+
+# new_list = "".join(map(str, list))
+start = len(list) - 1
+stop = -1
+step = -1
+
+for i in range(start, stop, step):
+    new_list.append(list[i])
+
+print(list)
+print(new_list)
+
+# len(list) - 1 #? da el indice del ultimo elemento de la lista
+# -1 #? es el punto de parada, lo que significa que el bucle se detendrá antes de alcanzar el índice -1.
+# -1 #? como el paso significa que el bucle disminuirá el índice en 1 en cada iteración, iterando efectivamente hacia atrás.
+
+
