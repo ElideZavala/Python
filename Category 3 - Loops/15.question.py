45d1d2d576588c237fbef016e39fda823f041986
Elide <zavalavinagreelide@gmail.com>
2025-05-20 21:57:03 -0600

    question 15

--- Category 3 - Loops/15.question.py
+++ Category 3 - Loops/15.question.py
@@ -7,7 +7,31 @@
 * * * * 
 * * * * * 
 * * * * 
-* * * 
+* * *
 * * 
 *
 """
+
+string = list("*")
+
+for i in range(0, 5, 1):
+    for j in range(0, -i, -1):
+        print("*", end=" ")
+    print("")
+
+# Va a empezr desde 0 que sera i, el menos i indica que ira hacia abajo, ejemplo si i es 2, este ira hacia abajo, por que igual le agregamos el conteno con -1 en cual indica que ira a la reversa. 
+#  el primero ira del 0 al 1, solo imprimira 1
+# el segundio ira de 0 al 2, imprimira 2
+# el tercero ira del 0 al 3, imprimira 3
+# y asi sucesibamentem siempre y cuando el conteo sea al revez. 
+
+for i in range(0, 5, 1):
+    for j in range(i, 5, 1):
+        print("*", end=" ")
+    print("")
+
+
+# va a empezar desde el 0 que sera i, luego seguira 1 que igual sera i, luego seguira 2 que igual sera i
+# siempre sera 6
+# va ir de 1 en 1
+# con end unimos cada loop
--- Category 3 - Loops/15.solution.py
+++ Category 3 - Loops/15.solution.py
@@ -2,10 +2,20 @@ rows = 5
 
 for i in range(0, rows):
     for j in range(0, i + 1):
-        print("*", end=" ")
+        print("*", end="  ")
     print("\n")
+
+# i sera igual a 0 y 0 + 1 = 1
+# i sera igual a 1 y 1 + 1 = 2 
+# i sera igual a 2 y 2 + 1 = 3 
+# i sera igual a 3 y 3 + 1 = 4 
 
 for i in range(rows, 0, -1):
     for j in range(0, i - 1):
-        print("*", end=" ")
+        print("*", end="  ")
     print("\n")
+
+# i sera igual a 5 y 5 - 1 = 4
+# i sera igual a 4 y 4 - 1 = 3 
+# i sera igual a 5 y 3 - 1 = 2 
+# i sera igual a 2 y 2 - 1 = 1 
