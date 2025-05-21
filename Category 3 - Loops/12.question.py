dfcb62af040f25cb7482362f5c86864cfdb2e9b1
Elide <zavalavinagreelide@gmail.com>
2025-05-20 20:26:38 -0600

    question 12

--- Category 3 - Loops/12.question.py
+++ Category 3 - Loops/12.question.py
@@ -1,1 +1,10 @@
 # Reverse an integer
+number = 3586540
+reversed_num = 0
+
+while number > 0:
+    remainder = number % 10 # Se obtiene el ultimo digito del numero actual   
+    reversed_num = (reversed_num * 10) + remainder # se multiplica reversed_num por 10 para desplazar los digitos existentes hacia la izquieda y luego se añade el remaider.
+    number = number // 10 # Se elimina el último dígito del número orginal.
+    
+print(reversed_num) # Se imprime el reversed_num que contendrá en número invertido.
