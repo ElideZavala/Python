d64b4eb3118539c37036a5003ce1e6fad27f75ec
Elide <zavalavinagreelide@gmail.com>
2025-06-04 21:56:25 -0600

    question 3

--- Category 5 - Strings/3.question.py
+++ Category 5 - Strings/3.question.py
@@ -1,3 +1,11 @@
 # Find all occurrences of “is” in a given string ignoring the case
 
 str = "My cat is awesome. You're cat is amazing. Is his cat asleep?"
+
+sub_string = "is"
+
+temp_string = str.lower() # Convertimos todo el texto en minuscula.
+
+count = temp_string.count(sub_string.lower()) # Vamos a contar la cantidad de veces que aparece "is" en la cadena, esa misma variable de busqueda, la convertimos en minuscula.
+
+print("🐛 ", count) # 4
