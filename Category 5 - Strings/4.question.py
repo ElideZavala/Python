9282f2424afe1ada4cccf8f3c482a9d04ad4ad93
Elide <zavalavinagreelide@gmail.com>
2025-06-04 22:20:41 -0600

    question 4

--- Category 5 - Strings/4.question.py
+++ Category 5 - Strings/4.question.py
@@ -1,3 +1,13 @@
 # Given an input string, count occurrences of all characters within a string
 
-str = "Lemon Malt"
+str = "Elide Zavala Vinagre"
+lower_str = str.lower()
+
+count_dict = dict()
+
+for char in lower_str:
+    count = lower_str.count(char)
+
+    count_dict[char] = count
+
+print(count_dict)
