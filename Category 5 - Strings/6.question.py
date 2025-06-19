ebea29eab86541bd498eb2a1cdc52145934e6fab
Elide <zavalavinagreelide@gmail.com>
2025-06-18 21:43:25 -0600

    question 6

--- Category 5 - Strings/6.question.py
+++ Category 5 - Strings/6.question.py
@@ -1,3 +1,10 @@
 # Find the last position of a substring “Kelly” in a given string
 
-str = "Kelly is a data scientist who knows Python. Kelly works at google."
+str = "Kelly is a data scientist who knows Python. Kelly works at google"
+
+index =  str.rfind('Kelly') # Encuentra la ultima position of Kell # 44
+print("****************************************")
+
+print("🐛 ", )
+print("****************************************")
+
