dd6a02ed98c2bdceec3afc0e535d44c8d4ff5975
Elide <zavalavinagreelide@gmail.com>
2025-06-28 21:38:42 -0600

    question 8

--- Category 5 - Strings/8.question.py
+++ Category 5 - Strings/8.question.py
@@ -1,3 +1,25 @@
 # Remove empty strings from a list of strings
 
 str_list = ["Emma", "Jon", "", "Kelly", None, "Eric", ""]
+tiene_vacios = False
+
+# for strs in str_list:
+#     if (strs == "" or strs is None or (isinstance(strs, str) and strs.strip() == "")):    
+#         print(strs, 'vacios')
+#         tiene_vacios = True
+#         break
+#     else:
+#         print(strs)
+
+# tiene_vacios = any(elemento == "" or elemento is None or (isinstance(elemento, str) and elemento.strip() == "") for elemento in str_list)
+
+# print(tiene_vacios)
+
+
+
+filter_none = list(filter(None, str_list))
+print(filter_none)
+
+
+
+
