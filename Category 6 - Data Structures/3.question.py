989e412f2ff214f542049831db40200ea13ee5a8
Elide <zavalavinagreelide@gmail.com>
2025-07-12 17:51:52 -0600

    question 3

--- Category 6 - Data Structures/3.question.py
+++ Category 6 - Data Structures/3.question.py
@@ -1,3 +1,12 @@
 # Iterate a given list and count the occurrence of each element and create a dictionary to show the count of each element
 
 sample_list = [11, 45, 8, 11, 23, 45, 23, 45, 89]
+
+counts_elements = {}
+
+for x in sample_list:
+    counts_elements[x] = sample_list.count(x)
+
+
+print("🐛 ", counts_elements)
+    
