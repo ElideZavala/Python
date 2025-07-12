fb3ae990d466ee8a0ad77d759b8702f94b95d5fc
Elide <zavalavinagreelide@gmail.com>
2025-07-12 16:33:56 -0600

    question 1

--- Category 6 - Data Structures/1.question.py
+++ Category 6 - Data Structures/1.question.py
@@ -2,3 +2,22 @@
 
 list_one = [3, 6, 9, 12, 15, 18, 21]
 list_two = [4, 8, 12, 16, 20, 24, 28]
+combined_list2 = list()
+
+ood_index = [list_one[i] for i in range(1, len (list_one), 2)] 
+even_index = [list_two[i] for i in range(0, len(list_two), 2)]
+combined_list = ood_index + even_index
+print(combined_list)
+
+print("****************************************")
+
+ood_index2 = list_one[1::2]
+even_index2 = list_two[0::2]
+combined_list2.extend(ood_index2)
+combined_list2.extend(even_index2)
+
+print(combined_list2)
+
+
+
+
