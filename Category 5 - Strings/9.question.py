30746f4544b1ceb4fafb87bbc692d267b4937c86
Elide <zavalavinagreelide@gmail.com>
2025-07-12 14:37:18 -0600

    question 9

--- Category 5 - Strings/9.question.py
+++ Category 5 - Strings/9.question.py
@@ -1,3 +1,8 @@
 # Removal all the characters other than integers from a string
 
 str = 'I am 25 years and 10 months old'
+
+
+result = "".join([item for item in str if item.isdigit()])
+print("🐛 result", result)
+
