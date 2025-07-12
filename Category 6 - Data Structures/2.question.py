e919567885fec492cb2d333fe218d0feea63ebd8
Elide <zavalavinagreelide@gmail.com>
2025-07-12 16:46:55 -0600

    question 2

--- Category 6 - Data Structures/2.question.py
+++ Category 6 - Data Structures/2.question.py
@@ -1,3 +1,10 @@
 # Given a list, remove the element at index 4 and add it to the 2nd position and at the end of the list
 
 list = [34, 54, 67, 89, 11, 43, 94]
+
+index_four = list.pop(4)
+
+# list[2] = index_four
+list.insert(2, index_four)
+list.append(index_four)
+print(list)
