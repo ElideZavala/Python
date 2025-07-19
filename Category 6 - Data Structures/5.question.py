6603311e57683e7ad6ef0db078ccd1831efe40b3
Elide <zavalavinagreelide@gmail.com>
2025-07-19 16:20:06 -0600

    question 5

--- Category 6 - Data Structures/5.question.py
+++ Category 6 - Data Structures/5.question.py
@@ -2,3 +2,20 @@
 
 set1 = {65, 42, 78, 83, 23, 57, 29}
 set2 = {67, 73, 43, 48, 83, 57, 29}
+
+# primera forma
+intersection = set1.intersection(set2)
+print("🐛 intersection", intersection)
+
+# segunda forma
+intersection_and = set1 & set2 
+#print("🐛 intersection_and ", intersection_and)
+
+#for i in intersection:
+#   set1.remove(i)
+
+set1.difference_update(intersection)
+
+print(set1)
+
+# remove_elements = set1.remove(intersection)
