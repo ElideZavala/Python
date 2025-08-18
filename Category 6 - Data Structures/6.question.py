f61b90d88f930a9f9512037bad2929280133da68
Elide <zavalavinagreelide@gmail.com>
2025-08-17 22:10:09 -0600

    question 6

--- Category 6 - Data Structures/6.question.py
+++ Category 6 - Data Structures/6.question.py
@@ -2,3 +2,25 @@
 
 first_set = {27, 43, 34}
 second_set = {34, 93, 22, 27, 43, 53, 48}
+
+
+print("****************************************")
+
+print("first subset", first_set.issubset(second_set) )
+print("second subset", second_set.issubset(first_set) )
+print("****************************************")
+
+print("first superset", first_set.issuperset(second_set) )
+print("SECOND superset", second_set.issuperset(first_set) )
+print("****************************************")
+
+if first_set.issubset(second_set):
+    first_set.clear()
+
+if second_set.issubset(first_set):
+    second_set.clear()
+print("****************************************")
+
+print("🐛 ", first_set)
+print("🐛 ", second_set)
+
