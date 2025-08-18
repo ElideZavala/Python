45496cb6e73ed16e42afe09607dcde9d27b89532
Elide <zavalavinagreelide@gmail.com>
2025-08-17 22:10:28 -0600

    question 8

--- Category 6 - Data Structures/8.question.py
+++ Category 6 - Data Structures/8.question.py
@@ -1,4 +1,8 @@
 # Given a dictionary get all values from the dictionary and add them to a list but don’t add duplicates
 
-speed = {'jan': 47, 'feb': 52, 'march': 47, 'April': 44,
-         'May': 52, 'June': 53, 'july': 54, 'Aug': 44, 'Sept': 54}
+from typing import List, Any
+
+speed = {'jan': 47, 'feb': 52, 'march': 47, 'April': 44, 'May': 52, 'June': 53, 'july': 54, 'Aug': 44, 'Sept': 54}
+
+list_of_values = list(set(speed.values()))
+print("🐛 list_of_values", list_of_values) # [44, 47, 52, 53, 54]
