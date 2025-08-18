8f545e44a127e039d047ca16da5a028664c9bb5d
Elide <zavalavinagreelide@gmail.com>
2025-08-17 22:10:19 -0600

    question 7

--- Category 6 - Data Structures/7.question.py
+++ Category 6 - Data Structures/7.question.py
@@ -2,3 +2,20 @@
 
 roll_number = [47, 64, 69, 37, 76, 83, 95, 97]
 sample_dict = {'John': 47, 'Emma': 69, 'Kelly': 76, 'Jason': 97}
+
+list_of_keys = list(sample_dict.values())
+
+set1 = set(roll_number)
+set2 = set(list_of_keys)
+
+desiguales = set1.symmetric_difference(set2)
+
+roll_number = [item for item in roll_number if item not in desiguales]
+
+print("🐛 roll_number", roll_number)
+
+
+
+
+
+
