f41f5e5ef502fd553dc59585369f47a00b5c1000
Elide <zavalavinagreelide@gmail.com>
2025-08-23 16:37:48 -0600

    question 4

--- Category 7 - Lists/4.question.py
+++ Category 7 - Lists/4.question.py
@@ -5,3 +5,15 @@ list1 = ["Hello ", "Hi "]
 list2 = ["Dear", "Sir"]
 
 ['Hello Dear', 'Hello Sir', 'Hi Dear', 'Hi Sir']
+
+
+list_concatenate = [x + y for x in list1 for y in list2]
+#for x in list2:
+ #   list_concatenate.append(list1[0] + x )
+
+#for z in list2:
+#    list_concatenate.append(list1[1] + x)
+    
+
+
+print(list_concatenate) # result = [x + y for x in list1 for y in list2]
