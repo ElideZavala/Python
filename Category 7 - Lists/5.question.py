d216c5980b43af1750830996c7b7a2ec1f20d8b3
ElideZavala <zavalavinagreelide@gmail.com>
2025-08-26 11:32:28 -0600

    Refactor code to remove empty strings from the list using filter

--- Category 7 - Lists/5.question.py
+++ Category 7 - Lists/5.question.py
@@ -4,4 +4,4 @@ list1 = [10, 20, 30, 40]
 list2 = [100, 200, 300, 400]
 
 for list1, list2 in zip(list1, reversed(list2)): 
-    print(list1, list2, sep=" ")
+    print(list1, list2, sep="")
--- Category 7 - Lists/6.question.py
+++ Category 7 - Lists/6.question.py
@@ -1,3 +1,13 @@
 # Remove empty strings from the list of strings
 
 list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
+# list1.remove("")
+
+# for i, elemento in enumerate(list1):
+#     if (elemento == ""):
+#         list1.remove(elemento)
+    
+
+list1 = list(filter(None, list1))
+print(list1)
+
