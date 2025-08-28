4d6ff4f09ce5225cb10082ac67b9e6890a53d36a
ElideZavala <zavalavinagreelide@gmail.com>
2025-08-28 09:00:30 -0600

    Add sub list ["h", "i", "j"] to the nested list

--- Category 7 - Lists/8.question.py
+++ Category 7 - Lists/8.question.py
@@ -1,6 +1,9 @@
 # Given a nested list extend it by adding the sub list ["h", "i", "j"] in such a way that it will look like the following list
 
 list1 = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
+list_complement = ["h", "i", "j"]
 
+list1[2][1][2].extend(list_complement)
+print(list1)
 
-['a', 'b', ['c', ['d', 'e', ['f', 'g', 'h', 'i', 'j'], 'k'], 'l'], 'm', 'n']
+# ['a', 'b', ['c', ['d', 'e', ['f', 'g', 'h', 'i', 'j'], 'k'], 'l'], 'm', 'n']
