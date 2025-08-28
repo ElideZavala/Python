877857ed3b739d7710e4108b31fdf67ac34018dd
ElideZavala <zavalavinagreelide@gmail.com>
2025-08-28 09:29:39 -0600

    Replace first occurrence of 20 with 200 in the list

--- Category 7 - Lists/9.question.py
+++ Category 7 - Lists/9.question.py
@@ -1,3 +1,9 @@
 # find value 20 in the list, and if it is present, replace it with 200. Only update the first occurrence of a value
 
 list = [5, 10, 15, 20, 25, 50, 20]
+
+number = 20
+index = list.index(number)
+list[index] = 200
+
+print(list)
