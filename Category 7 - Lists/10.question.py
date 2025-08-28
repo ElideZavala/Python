5de4aaa6d81fdbf128c5d100e88ef7b2341da54e
ElideZavala <zavalavinagreelide@gmail.com>
2025-08-28 09:49:35 -0600

    Refactor code to remove all occurrences of 20 from the list using a while loop

--- Category 7 - Lists/10.question.py
+++ Category 7 - Lists/10.question.py
@@ -1,3 +1,15 @@
 # remove all occurrence of 20 from the list
 
 list = [5, 20, 15, 20, 25, 50, 20]
+
+while 20 in list:
+    list.remove(20)
+
+print(list) # [5, 15, 25, 50]
+
+# alternativa
+def remove_value(sample_list, val):
+    return [value for value in sample_list if value != val]
+
+
+result_list = remove_value(list, 20) # [5, 15, 25, 50]
