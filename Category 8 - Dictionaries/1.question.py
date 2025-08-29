a0fcee3d92b7f28571779e07596f0bdac77e87d4
ElideZavala <zavalavinagreelide@gmail.com>
2025-08-29 09:29:06 -0600

    Add dictionary creation and print statement for key-value pairs

--- Category 8 - Dictionaries/1.question.py
+++ Category 8 - Dictionaries/1.question.py
@@ -2,3 +2,10 @@
 
 keys = ['Ten', 'Twenty', 'Thirty']
 values = [10, 20, 30]
+
+new_dictionary = dict(zip(keys, values))
+
+print('new_dictionary: ', new_dictionary) 
+# Output: {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+
+
