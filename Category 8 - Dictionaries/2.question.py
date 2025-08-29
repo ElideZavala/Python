eccfe9f80721abbff5f8cde72a1d5b449ee288b5
ElideZavala <zavalavinagreelide@gmail.com>
2025-08-29 09:50:00 -0600

    Merge two dictionaries using unpacking method and add print statement

--- Category 8 - Dictionaries/2.question.py
+++ Category 8 - Dictionaries/2.question.py
@@ -2,3 +2,11 @@
 
 dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
 dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+# metodo 1
+# merge_dict = dict1 | dict2
+
+# metodo 2
+merge_dict = {**dict1, **dict2}
+print('merge_dict: ', merge_dict)
+
