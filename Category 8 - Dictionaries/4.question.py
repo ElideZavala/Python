38a3555e224955269e6b3bba48239d899bda6831
ElideZavala <zavalavinagreelide@gmail.com>
2025-08-29 10:06:50 -0600

    Add dictionary creation using fromkeys and print statement for employee defaults

--- Category 8 - Dictionaries/4.question.py
+++ Category 8 - Dictionaries/4.question.py
@@ -1,4 +1,12 @@
 # Initialize dictionary with default values
+import json
 
 employees = ['Kelly', 'Emma', 'John']
 defaults = {"designation": 'Application Developer', "salary": 8000}
+
+create_dictionary = dict.fromkeys(employees, defaults)
+
+# for name in employees:
+
+print(json.dumps(create_dictionary, indent=4, ensure_ascii = False))
+
