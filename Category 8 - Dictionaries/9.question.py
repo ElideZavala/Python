99d98e7118b4817689a2089d5962d45476f83fcf
Elide <zavalavinagreelide@gmail.com>
2025-09-17 21:38:48 -0600

    question 9 `

--- Category 8 - Dictionaries/9.question.py
+++ Category 8 - Dictionaries/9.question.py
@@ -5,3 +5,13 @@ sample_dict = {
     'Math': 65,
     'history': 75
 }
+
+# get_minimun = {k: v for (k,v) in sample_dict.items() if v == min(v)}
+get_minimun = min(sample_dict.values())
+get_key = {k: v for (k,v) in sample_dict.items() if v == get_minimun}
+
+get_key2 = {k: v for (k,v) in sample_dict.items() if v == min(sample_dict.values())}
+
+print(get_key2)
+print(get_key)
+
