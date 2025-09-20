8860a01c99e28374ed1a4f9a6100b024f781da10
Elide <zavalavinagreelide@gmail.com>
2025-09-20 13:57:44 -0600

    set 2 question

--- Category 9 - Sets/2.question.py
+++ Category 9 - Sets/2.question.py
@@ -3,5 +3,5 @@
 set_one = {10, 20, 30, 40, 50}
 set_two = {30, 40, 50, 60, 70}
 
-merge_set = set_one.intersection(set_two)
-print(merge_set)
+merge_set = set_one.intersection(set_two) 
+print(merge_set) # {40, 50, 30}
