3157e35721ecf0ee08c7f722a2b93a2f24d43a58
Elide <zavalavinagreelide@gmail.com>
2025-09-20 15:41:26 -0600

    set question 5

--- Category 9 - Sets/5.question.py
+++ Category 9 - Sets/5.question.py
@@ -1,3 +1,13 @@
 # Remove items 10, 20, 30 from the following set at once
 
 set = {10, 20, 30, 40, 50}
+list_D= [10, 20, 30]
+
+#1
+set.difference_update({10, 20, 30})
+
+#2
+# for i in list_D:
+#     set.discard(i)
+
+print(set)
