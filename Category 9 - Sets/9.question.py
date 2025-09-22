fc3e547882f5a52a1669c9c3f21309822f84710b
Elide <zavalavinagreelide@gmail.com>
2025-09-22 10:17:27 -0600

    set question 9

--- Category 9 - Sets/9.question.py
+++ Category 9 - Sets/9.question.py
@@ -2,3 +2,8 @@
 
 set_one = {10, 20, 30, 40, 50}
 set_two = {30, 40, 50, 60, 70}
+
+# Actualizar un conjunto con la intersección de sí mismo y otro.
+# Regresa elemenos que son comunes entre si.
+set_one.intersection_update(set_two)
+print(set_one)
