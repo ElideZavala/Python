4e8c77765fc9790f363c4ce2b636ebd18acddfff
Elide <zavalavinagreelide@gmail.com>
2025-11-17 20:32:33 -0600

    question 2 datetime

--- Category 11 - Data Time/2.solution.py
+++ Category 11 - Data Time/2.solution.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 
+date_string = "Feb 25 2020 4:20PM"
+fecha = datetime.strptime(date_string, "%d %m %Y %I:%M%p")
 
-date_string = "Feb 25 2300 4:20PM"
-datetime_obj = datetime.strptime(date_string, "%b %d %Y %I:%M%p")
+# date_string = "Feb 25 2300 4:20PM"
+# datetime_obj = datetime.strptime(date_string, "%b %d %Y %I:%M%p")
 
 print(datetime_obj)
