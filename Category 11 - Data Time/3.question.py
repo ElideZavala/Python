7357234b321dbb1a113af3d6e6c467c7387835e4
Elide <zavalavinagreelide@gmail.com>
2025-11-17 21:07:20 -0600

    Implement date manipulation in question 3 by subtracting a week from a given date

--- Category 11 - Data Time/3.question.py
+++ Category 11 - Data Time/3.question.py
@@ -1,3 +1,13 @@
 # Subtract a week (7 days)  from a given date.s
 
-# given_date = datetime(2100, 2, 25)
+from datetime import datetime, timedelta
+
+# year, month, day
+given_date = datetime(2025, 11, 17) # lo convertimos a datetime
+
+new_date = given_date - timedelta(days=7) # le restamos el datetime nuestro, menos los dias.
+# restamos dias - timedelta(days=7)
+# restamos semanas - timedelta(weeks=1)
+
+print("Fecha original:", given_date) # 2025-11-17 00:00:00
+print("Fecha después de restar una semana:", new_date) # 2025-11-10 00:00:00
