4aa690e1721b8392b693a44543afc5a58f617375
Elide <zavalavinagreelide@gmail.com>
2025-11-17 21:48:08 -0600

    Add date formatting and current date display in question 4

--- Category 11 - Data Time/4.question.py
+++ Category 11 - Data Time/4.question.py
@@ -1,5 +1,12 @@
 # Print a date in a the following format
 
 # Day_name  Day_number  Month_name  Year
+from datetime import datetime
 
-# given_date = datetime(2100, 12, 15)
+today = datetime.now() 
+print(today) # 2025-11-17 21:41:38.381537
+
+formatted_date = today.strftime("%A %d %B %Y")
+print(formatted_date) # Monday 17 November 2025
+
+# given_date = datetime(2025, 11, 17)
