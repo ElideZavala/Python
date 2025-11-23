c3f28e23b70f4fe357a8d6037da4e0d5d07aa386
Elide <zavalavinagreelide@gmail.com>
2025-11-23 17:53:08 -0600

    Add date manipulation to question 6 by adding a week and 12 hours to a given date

--- Category 11 - Data Time/6.question.py
+++ Category 11 - Data Time/6.question.py
@@ -1,3 +1,9 @@
 # Add a week (7 days) and 12 hours to a given date
 
-# given_date = datetime(2050, 03, 15, 10, 0, 0)
+from datetime import datetime, timedelta
+
+given_date = datetime(2050, 3, 15, 10, 0, 0)
+new_date = given_date + timedelta(days=7, hours=12)
+print(new_date) # 2050-03-22 22:00:00
+
+
