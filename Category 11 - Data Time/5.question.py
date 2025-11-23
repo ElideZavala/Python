ec14e8b7a50b577733a10322b5e03d08d6e271af
Elide <zavalavinagreelide@gmail.com>
2025-11-23 17:52:54 -0600

    Add functionality to determine the day of the week for a specific date in question 5

--- Category 11 - Data Time/5.question.py
+++ Category 11 - Data Time/5.question.py
@@ -1,3 +1,7 @@
 # Find the day of the week of a given date
 
+from datetime import datetime
 # given_date = datetime(2100, 12, 15)
+
+day = datetime(2100, 12, 15).strftime("%A")
+print(day) # Wednesday
