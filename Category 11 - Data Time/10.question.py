a307285803ef7adf375e467819ca5bbc12814cca
Elide <zavalavinagreelide@gmail.com>
2025-12-01 15:59:59 -0600

    Add date comparison and calculation of days between two dates in question 10

--- Category 11 - Data Time/10.question.py
+++ Category 11 - Data Time/10.question.py
@@ -1,6 +1,17 @@
 # Calculate number of days between two given dates
 
+from datetime import datetime
 
-# date_1 = datetime(2050, 2, 25)
+date_1 = datetime(2050, 2, 25)
+date_2 = datetime(2065, 9, 17)
+delta = None
 
-# date_2 = datetime(2065, 9, 17)
+if date_1 > date_2:
+    print("Date 1 is greater")
+    delta = date_1 - date_2
+else:
+    print("Date 2 is greater")
+    delta = date_2 - date_1
+
+print("Number of Days:", delta.days)
+print("Number of Days:", delta)
