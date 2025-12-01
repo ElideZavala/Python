ecc44aefb2d7f3125a4dea9266b6dc012e0dce37
Elide <zavalavinagreelide@gmail.com>
2025-12-01 14:20:14 -0600

    Add current time and milliseconds calculation in question 7

--- Category 11 - Data Time/7.question.py
+++ Category 11 - Data Time/7.question.py
@@ -1,1 +1,15 @@
 # Print current time in milliseconds
+from datetime import datetime
+
+date_now = datetime.now()
+print(date_now)
+
+# miliseconds = int(date_now.timestamp() * 1000)
+microseconds = int(date_now.strftime("%f"))
+milliseconds_from_time = microseconds // 1000
+
+print(milliseconds_from_time, "milisegundos") 
+
+# timestamp 
+
+
