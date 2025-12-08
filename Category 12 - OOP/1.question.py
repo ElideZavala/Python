ca8f10f2b8b911e2b030107a8f97e2d0d7c60ada
Elide <zavalavinagreelide@gmail.com>
2025-12-07 19:02:22 -0600

    Add Vehicle class with max_speed and mileage attributes in question 1

--- Category 12 - OOP/1.question.py
+++ Category 12 - OOP/1.question.py
@@ -1,1 +1,10 @@
 # Create a Vehicle class with max_speed and mileage instance attributes
+
+class Vehicule:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage   = mileage
+
+mi_vehiculo = Vehicule(220, 90)
+print(f"mi carro corre a {mi_vehiculo.max_speed} con un kilometraje de {mi_vehiculo.mileage}")
+        
