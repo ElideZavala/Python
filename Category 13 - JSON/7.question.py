0e7216e47e6a58cdf32e6f8d60c926d620b356f0
Elide <zavalavinagreelide@gmail.com>
2026-01-19 09:39:05 -0600

    question 7

--- Category 13 - JSON/7.question.py
+++ Category 13 - JSON/7.question.py
@@ -1,7 +1,7 @@
 # Parse the following JSON to get all the values of a key ‘name’ within an array
+import json
 
-
-[
+list = """[
     {
         "id": 1,
         "name": "name1",
@@ -19,3 +19,15 @@
         ]
     }
 ]
+"""
+
+data = []
+
+try:
+    data = json.loads(list) # Convierte una cadena JSON en un objeto de Python
+except:
+    print('no ser obtubieron todos los valores.')
+
+data_list = [item.get('name') for item in data] # ['name1', 'name2']
+
+print(data_list)
