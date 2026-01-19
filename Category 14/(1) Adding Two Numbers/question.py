d39093884258ea737864db8d517da2e623213928
Elide <zavalavinagreelide@gmail.com>
2026-01-19 09:50:05 -0600

    (1) Adding Two Numbers

--- Category 14/(1) Adding Two Numbers/question.py
+++ Category 14/(1) Adding Two Numbers/question.py
@@ -1,2 +1,6 @@
 # ----------------- Program Task Description
 # Write a program that takes two numbers from the user, adds them together and shows the result to the user
+numberOne = int(input("Escribe el primer numero para la suma = "))
+numberTwo = int(input("Escribe el segundo numero para la suma = "))
+adds = numberOne + numberTwo
+print(f"Total = {adds}")
